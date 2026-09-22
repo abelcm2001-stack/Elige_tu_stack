6f478a43989036069c2aa1c288d7f5d87ca21ee7
abelcm2001-stack <263982732+abelcm2001-stack@users.noreply.github.com>
2026-09-22 13:23:06 +0200

    I have declared the variables.

--- fichero_demostracion.php
+++ fichero_demostracion.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP</title>
 </head>
 <body>
-    
+
+    <?php echo $fechaActual = date('d/m/Y'); ?>
+    <?php echo $horaActual = date('H:i:s'); ?>
+
+
+    <p> Fecha de hoy: <?php echo htmlspecialchars($fechaActual); ?> </p>
+    <p> Hora actualmente: <?php echo htmlspecialchars($horaActual); ?> </p>
+
 </body>
 </html>
